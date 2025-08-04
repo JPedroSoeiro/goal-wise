094939d751a7515b7a9146b24760bec46d3a47d5
João Pedro Soeiro Lemos <jpsoeirolemos@gmail.com>
2025-08-04 16:49:31 -0300

    att package-lock

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,8 +1,6 @@
-import { db } from "./index";
-// src/db/index.ts
-import { drizzle } from "drizzle-orm/node-postgres"; // Importação corrigida
+import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
-import * as schema from "./schema"; // Importa todas as suas definições de esquema
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not set");
